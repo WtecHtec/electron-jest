cbf9b51f74b71d92647aa86f16c98e4f49dc4fd6
herzshen <herzshen@tencent.com>
2024-01-04 16:45:30 +0800

    chrome配置

--- jest-chrome-plugin/src/main.jsx
+++ jest-chrome-plugin/src/main.jsx
@@ -5,12 +5,12 @@ import { ConfigProvider } from 'antd'
 import zhCN from 'antd/locale/zh_CN'
 // 全局样式
 import '@/common/styles/frame.styl'
-import Popup from '@/popup'
+// import Popup from '@/popup'
 // 在popup页面调试content script，仅用于开发环境，build前记得要注释掉。
 // import '@/content'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <ConfigProvider locale={zhCN}>
-        <Popup />
+        {/* <Popup /> */}
     </ConfigProvider>
 )
